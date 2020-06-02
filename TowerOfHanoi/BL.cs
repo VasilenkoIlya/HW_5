39201a7600276ae78c3cc5244813b5470ffd90f2
ilya.usaty <ilya.usaty@gmail.com>
2020-06-03 00:49:38 +0300

    I am implemented Tower of hanoi recursively

---
+++ TowerOfHanoi/BL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerOfHanoi
+{
+    class BL
+    {
+        public static void MoveTower(int high, int colFrom, int colTo)
+        {
+            if (high == 1)//Тривиальный случай
+            {
+                UI.TrivialOccasion(colFrom, colTo);
+            }
+            else
+            {
+                int tmp = 6 - colFrom - colTo;//вычисление номера временного столбца
+                MoveTower(high - 1, colFrom, tmp);//перекладываем диск с начального столбца на временный
+                UI.MoveDisk(high, colFrom, colTo);
+                MoveTower(high - 1, tmp, colTo);//перекладываем диск с временного столбца на конечный
+            }
+        }
+    }
+}
---
+++ TowerOfHanoi/UI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerOfHanoi
+{
+    class UI
+    {
+        public static void TrivialOccasion(int colFrom, int colTo)
+        {
+            Console.WriteLine("Move disk 1 from column {0} to column {1}", colFrom, colTo);
+        }
+
+        public static void MoveDisk(int high, int colFrom, int colTo)
+        {
+            Console.WriteLine("Move disk {0} from column {1} to column {2}", high, colFrom, colTo);
+        }
+    }
+}
